393e90fe706a4b7e43ab7d594a0506156d6f7f2d
snel <fransnellagirousse@gmail.com>
2023-06-21 21:38:43 +0300

    ajout du code sur JS

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,32 @@
-$(document).ready(function(){
-    $.ajax({
-      url: "https://batman-api.sayna.space/questions",
-      dataType: 'json',
-      success:function(questions){
-        console.log(questions[0]);
-      },
-      error:function(){
-        console.log('impossible de se connecter');
-      }
-    });
+$(document).ready(function () {
+ const demarrer = document.getElementById('demarrer');
+ const nextButton = document.getElementById("question-suivante");
+
+ demarrer.addEventListener('click', (e) => {
+  e.preventDefault;
+  showQuestions();
+  $ajax({
+    url: "https://batman-api.sayna.space/questions",
+    dataType: "json",
+    success: function (quizz) {
+     for (let i = 0; i < quizz.length; i++) {
+      console.log(quizz[i].respons);
+
+     }
+    },
+    error : function() {
+      console.log ('Prebleme au niveau de serveur ')
+    }
+
+  });
+
+ })
 });
+function showQuestions() {
+  const question = document.getElementById('questions');
+  question.style.display = 'block';
+  const demarrer = document.getElementById('entete');
+  demarrer.style.display = 'none';
+  $show('question-suivante').show();
+}
+
