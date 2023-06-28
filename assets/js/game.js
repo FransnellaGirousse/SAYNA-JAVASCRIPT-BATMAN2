c152f01a76f88d79407330c066196f22dd4f4357
snel <fransnellagirousse@gmail.com>
2023-06-28 16:57:20 +0300

    ajout du code js

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,6 +22,43 @@ $(document).ready(function () {
 
  })
 });
+
+nextButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  validerQuestion();
+  afficherQuestionSuivante();
+});
+
+function validerQuestion() {
+  // Logique de validation de la question actuelle
+  // Vérifier si la réponse de l'utilisateur est correcte, etc.
+  // Vous pouvez accéder aux éléments de la question actuelle en utilisant leur ID ou en les stockant dans des variables.
+
+  // Exemple : Vérifier si la réponse de l'utilisateur est correcte
+  const reponseUtilisateur = document.getElementById(
+    "reponse-utilisateur"
+  ).value;
+  const reponseCorrecte = document.getElementById("reponse-correcte").value;
+
+  if (reponseUtilisateur === reponseCorrecte) {
+    console.log("Bonne réponse!");
+  } else {
+    console.log("Mauvaise réponse!");
+  }
+}
+
+function afficherQuestionSuivante() {
+  // Logique pour afficher la question suivante
+  // Vous pouvez cacher la question actuelle et afficher la question suivante
+
+  const questionActuelle = document.getElementById("question-actuelle");
+  const questionSuivante = document.getElementById("question-suivante");
+
+  questionActuelle.style.display = "none";
+  questionSuivante.style.display = "block";
+}
+
+
 function showQuestions() {
   const question = document.getElementById('questions');
   question.style.display = 'block';
